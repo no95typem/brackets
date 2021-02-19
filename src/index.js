c787850dfc98c08eaf453a85918d51142cfedda3
no95typem <no95typem@gmail.com>
2021-02-19 19:43:55 +0400

    feat: add check() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig, pos, nextCorrectClosingBracket) {
+    if(typeof(pos)=="undefined") pos = 0;
+    mainloop: for(pos; pos < str.length; pos++) {
+        if(typeof(nextCorrectClosingBracket)!="undefined" &&
+        str[pos]==nextCorrectClosingBracket) return pos;
+        for(let j = 0; j < bracketsConfig.length; j++) {
+            // if there is open bracket
+            if(bracketsConfig[j][0]==str[pos]) {
+                pos++;
+                let result = check(str, bracketsConfig, pos, bracketsConfig[j][1]);
+                if(!result) return false;
+                else pos = result;
+                continue mainloop;
+            }
+        }
+        return false;
+    }
+    if(typeof(nextCorrectClosingBracket)=="undefined") return true;
+    else return false;
 }
